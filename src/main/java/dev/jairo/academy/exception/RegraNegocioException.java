e9c4bc7b0f320176910565d07c10cad0ff698d42
Jairo <jairodyne@gmail.com>
2026-05-20 18:31:09 -0300

    Added validations and pattern for error messages.

---
+++ src/main/java/dev/jairo/academy/exception/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package dev.jairo.academy.exception;
+
+public class RegraNegocioException extends RuntimeException {
+    public RegraNegocioException(String message) {
+        super(message);
+    }
+}
